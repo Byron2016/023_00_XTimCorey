f05b791ca05faa2d7feae5f4b64d3188894873f6
Byron 2016 <bgva2005@yahoo.com>
2021-10-28 07:44:45 -0500

    001_SRP_001_Inicial

---
+++ Sol_Console_SOLID/001_SRP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _001_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a mi aplicación");
+
+            // Preguntar por información del usuario
+            Person user = new Person();
+
+            Console.WriteLine("Cuál es su primer nombre: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Cuál es su apellido: ");
+            user.LastName = Console.ReadLine();
+
+            // Chequear que nombre y apellido son válidos.
+            if (string.IsNullOrEmpty(user.FirstName))
+            {
+                Console.WriteLine("No ha proporcionado un nombre inválido!");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(user.LastName))
+            {
+                Console.WriteLine("No ha proporcionado un apellido inválido!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Crear un nombre de usuario para la persona.
+            Console.WriteLine($"Su nombre de usuario es {user.FirstName.Substring(0,1) }{ user.LastName }");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
