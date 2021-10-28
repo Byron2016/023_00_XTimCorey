52e961950a5140995bb1a51d101c7410cc972c87
Byron 2016 <bgva2005@yahoo.com>
2021-10-28 08:00:53 -0500

    001_SRP_003_Agregar captura de información persona

---
+++ Sol_Console_SOLID/001_SRP/PersonDataCapture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_SRP
+{
+    public class PersonDataCapture
+    {
+        public static Person Capture()
+        {
+            // Preguntar por información del usuario
+            Person output = new Person();
+
+            Console.WriteLine("Cuál es su primer nombre: ");
+            output.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Cuál es su apellido: ");
+            output.LastName = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
