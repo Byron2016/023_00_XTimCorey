7a8523d9a7adbbc0b439bd4ce817e86eaa71f0d8
Byron 2016 <bgva2005@yahoo.com>
2021-10-28 07:54:12 -0500

    001_SRP_002_Agregar mensages stándares

---
+++ Sol_Console_SOLID/001_SRP/StandardMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_SRP
+{
+    public class StandardMessages
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Bienvenido a mi aplicación");
+        }
+
+        public static void EndApplication()
+        {
+            Console.ReadLine();
+        }
+    }
+}
